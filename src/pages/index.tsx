df8b02eb335036ecbd8f4f197ae96885b420312d
Sandoche <sandoche@protonmail.com>
2023-01-24 21:31:18 +0000

    Default colors set up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Navbar />
       <section>
         <Container>
